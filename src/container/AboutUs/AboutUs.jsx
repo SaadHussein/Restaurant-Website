8b619ef4d82894178777fd4466787b1bd1e4881e
SaadHussein <sa3dhusseein@gmail.com>
2023-07-26 04:04:53 +0300

    finish aboutus and menu

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,11 +1,51 @@
-import React from 'react';
+import React from "react";
 
-import './AboutUs.css';
+import "./AboutUs.css";
+import images from "../../constants/images";
 
-const AboutUs = () => (
-  <div>
-    AboutUs
-  </div>
-);
+const AboutUs = () => {
+  return (
+    <div
+      className="app__aboutus flex__center section__padding app__bg"
+      id="about"
+    >
+      <div className="app__aboutus-overlay flex__center">
+        <img src={images.G} alt="G letter" />
+      </div>
+
+      <div className="app__aboutus-content flex__center">
+        <div className="app__aboutus-content_about">
+          <h1 className="headtext__cormorant">About Us</h1>
+          <img src={images.spoon} alt="Spoon Image" className="spoon__img" />
+          <p className="p__opensans">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis
+            pharetra adipiscing ultrices vulputate posuere tristique. In sed
+            odio nec aliquet eu proin mauris et.
+          </p>
+          <button type="button" className="custom__button">
+            Know More
+          </button>
+        </div>
+
+        <div className="app__aboutus-content_knife flex__center">
+          <img src={images.knife} alt="Knife Image" />
+        </div>
+
+        <div className="app__aboutus-content_history">
+          <h1 className="headtext__cormorant">Our History</h1>
+          <img src={images.spoon} alt="Spoon Image" className="spoon__img" />
+          <p className="p__opensans">
+            Adipiscing tempus ullamcorper lobortis odio tellus arcu volutpat.
+            Risus placerat morbi volutpat habitasse interdum mi aliquam In sed
+            odio nec aliquet.
+          </p>
+          <button type="button" className="custom__button">
+            Know More
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default AboutUs;
